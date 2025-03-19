f554340a398e06c3f36ee6fec43ba83b068262a6
tuanaakyazi <157962239+tuanaakyazi@users.noreply.github.com>
2025-03-20 02:14:03 +0300

    Update asal_mi.py

--- asal_mi.py
+++ asal_mi.py
@@ -1,32 +1,18 @@
-def hesap_makinesi(sayi1, sayi2, islem):
-    if islem == "+":
-        sonuc = sayi1 + sayi2
-        return (f"{sayi1} + {sayi2} = {sonuc}")
-    elif islem == "-":
-        sonuc  = sayi1 - sayi2
-        return (f"{sayi1} - {sayi2} = {sonuc}")
-    elif islem == "*":
-        sonuc = sayi1 * sayi2
-        return (f"{sayi1} * {sayi2} = {sonuc}")
-    elif islem == "/":
-        if sayi2 == 0:
-            return "Hata: Bölme işlemi için ikinci sayi 0 olamaz!"
-        sonuc = sayi1 / sayi2
-        return (f"{sayi1} / {sayi2} = {sonuc}")
-    else :
-        return "Hata: Geçersiz işlem türü!"
-
+def asal_mi(sayi):
+    asal = True
+    if sayi < 2:
+        asal = False
+    for i in range(2,sayi//2 + 1):
+        if sayi % i == 0:
+             asal = False
+             break
+    return asal
 def main():
-    while True:
-        sayi1 = float(input("Lütfen 1. sayiyi giriniz : "))
-        sayi2 = float(input("Lütfen 2. sayiyi giriniz : "))
-        islem = input("Lütfen yapmak istediginiz islemi giriniz (+, -. /, *) : ")
-        sonuc =  hesap_makinesi(sayi1, sayi2, islem)
-        print(f"{sonuc}")
-        secim = input("devam etmek istiyor musunuz(E/H) : ")
-        if secim == "E" or secim == "e":
-            continue
-        else:
-            break
-if __name__ == "__main__":
+    sayi = int(input("Lütfen bir sayi giriniz : "))
+    if asal_mi(sayi):
+        print("bu sayi asal.")
+    else:
+        print("bu sayi asal degil.")
+
+if __name__== "__main__":
     main()
